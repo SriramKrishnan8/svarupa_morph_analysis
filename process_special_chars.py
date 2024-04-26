483d23dc226464eff7eb80c59e58724623edd962
Sriram Krishnan <sriramk8@gmail.com>
2024-04-26 17:16:16 +0530

    Remove svaras and other characters efficiently; Produces results into four temporary files - modified, segmented, sandhied, hyphenated; added temp folder to git ignore

--- process_special_chars.py
+++ process_special_chars.py
@@ -7,7 +7,11 @@ from tqdm import tqdm
 
 import handle_iti as iti
 
-script, in_, out_ = sys.argv
+script, in_, out_mo, out_se, out_sa, out_hy = sys.argv
+
+
+svaras = [ '\uA8E1', '\uA8E2', '\uA8E3', '\uA8E4', '\uA8E5', '\uA8E6', '\uA8E7', '\uA8E8', '\uA8E9', '\uA8E0', '\uA8EA', '\uA8EB', '\uA8EC', '\uA8EE', '\uA8EF', '\u030D', '\u0951', '\u0952', '\u0953', '\u0954', '\u0945' ]
+special_characters = [ '\uf15c', '\uf193', '\uf130', '\uf1a3', '\uf1a2', '\uf195', '\uf185', '\u200d', '\u200c', '\u1CD6', '\u1CD5', '\u1CE1', '\u030E', '\u035B', '\u0324', '\u1CB5', '\u0331', '\u1CB6', '\u032B', '\u0308', '\u030D', '\u0942', '\uF512', '\uF693', '\uF576', '\uF11E', '\u1CD1', '\u093C', '\uF697', '\uF6AA', '\uF692' ]
 
 
 def replace_chandrabindu(cb, input_string):
@@ -52,23 +56,52 @@ def remove_non_unicode(text):
     
     return cleaned_string
     
+    
+def replace_avagraha(input_string):
+    """ Replaces avagraha with hyphen
+    """
+    
+    text = input_string
+    text = text.replace("ऽ", "-")
+    
+    return text
+
+
+def remove_svara_iter(text):
+    """ Removes svaras by iterating through the text
+    """
+    
+    new_text = []
+    
+    for char in text:
+        if char in (svaras + special_characters):
+            continue
+        new_text.append(char)
+   
+    modified_text = "".join(new_text)
+    
+    return modified_text
+    
+
+def remove_svara_regexp(with_svara):
+    """ Removes svaras using regexp module
+    """
+
+    pattern = re.compile(r'[\uA8E1\uA8E2\uA8E3\uA8E4\uA8E5\uA8E6\uA8E7\uA8E8\uA8E9\uA8E0\uA8EA\uA8EB\uA8EC\uA8EE\uA8EF\u0945\u0951\u0952\u0953\u0954\uf15c\uf193\uf130\uf1a3\uf1a2\uf195\uf185\u200d\u200c\u1CD6\u1CD5\u1CE1\u030E\u035B\u0324\u1CB5\u0331\u1CB6\u032B\u0308\u030D\u0942\uF512\uF693\uF576\uF11E\u1CD1\u093C\uF697\uF6AA\uF692]')
+#    pattern = re.compile(r'[꣡꣫\uf15c\uf193\uf1a3\uf1a3꣯꣣꣡\uf195\uf1a2\uf185꣬᳡़꣯꣢꣣ॅ꣫\u200d\u200c\uF512\u1CD6\u1CD5\u1CE1\uA8EB\u030E\u035B\u0324\uA8E2\u1CB5\uA8EC\u0331\u1CB6\uA8F1\u032B\uA8EF\uA8E3\uA8E1\u0308\u030D\u0942\u0951\u0952\u0953\u0954]')
+    without_svara = pattern.sub('', with_svara)
+
+    return without_svara
+
 
 def remove_svara(text):
     """ Removes svaras
     """
 
-    new_text = []
-    for char in text:
-        if '\u0951' <= char <= '\u0954':
-            continue
-        # To remove zero width joiner character
-        elif char in ['\u200d', '\u200c', '\u1CD6', '\u1CD5', '\u1CE1']:
-            continue
-        new_text.append(char)
+    # Both remove_svara_iter and remove_svara_regexp functions work well
+    # but we are using remove_svara_regexp
     
-    modified_text = "".join(new_text)
-    
-    return modified_text
+    return remove_svara_regexp(text)
 
 
 def replace_others(input_string):
@@ -100,9 +133,14 @@ all_text = file_.read()
 file_.close()
 
 all_lines = list(filter(None, all_text.split("\n")))
-new_list = []
+# new_list = []
 
 iti_entries_dict = iti.get_iti_strings()
+
+modified_list = []
+segmented_list = []
+sandhied_list = []
+hyphenated_list = []
 
 print("Cleaning terms...")
 for i in tqdm(range(len(all_lines))):
@@ -111,15 +149,36 @@ for i in tqdm(range(len(all_lines))):
     new_text = remove_non_unicode(new_text)
     new_text = check_chandrabindu(new_text)
     new_text = replace_others(new_text)
+    new_text = replace_avagraha(new_text)
+
+    # Insert sandhi module
     
     cleaned_term = new_text
     segmented_term, sandhied_term, hyphenated_term = iti.replace_iti(
         new_text, iti_entries_dict
     )
-    new_list.append((line, cleaned_term, sandhied_term, hyphenated_term))
+    
+    # Instead of saving all forms, only the sandhied and hyphenated are stored
+    # new_list.append((line, cleaned_term, sandhied_term, hyphenated_term))
+    modified_list.append(cleaned_term)
+    segmented_list.append(segmented_term)
+    sandhied_list.append(sandhied_term)
+    hyphenated_list.append(hyphenated_term)
 
-updated_list = ["\t".join(item) for item in new_list]
+# updated_list = ["\t".join(item) for item in new_list]
 
-file_ = open(out_, 'w', encoding="utf-8")
-file_.write("\n".join(updated_list))
+file_ = open(out_mo, 'w', encoding="utf-8")
+file_.write("\n".join(modified_list))
+file_.close()
+
+file_ = open(out_se, 'w', encoding="utf-8")
+file_.write("\n".join(segmented_list))
+file_.close()
+
+file_ = open(out_sa, 'w', encoding="utf-8")
+file_.write("\n".join(sandhied_list))
+file_.close()
+
+file_ = open(out_hy, 'w', encoding="utf-8")
+file_.write("\n".join(hyphenated_list))
 file_.close()
