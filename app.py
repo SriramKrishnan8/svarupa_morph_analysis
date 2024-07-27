3357270c34e1d987bac890338e4002ccf849d888
Sriram Krishnan <sriramk8@gmail.com>
2024-07-27 22:50:51 +0530

    Modified app.py and updated example for deployment in the server

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask, request, Response
 import logging
+from flask_socketio import SocketIO
 
 import json
 
-#sys.path.append("./vedic_morph_analyser_sh")
-#sys.path.append("./sh_to_term_json")
-
 from vedic_morph_analyser_sh.wsmp_sh import run_sh_text, cgi_file
 from sh_to_term_json.generate_wsmp_results import generate_results
+from cleaning import clean_all
 
 # Replace the path with the SKT path
-cgi_file = "/usr/lib/cgi-bin/SKT_Experimental/sktgraph2"
+cgi_file = "/usr/lib/cgi-bin/SKT/sktgraph2"
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route('/sh-wsmp', methods=['GET'])
 def wsmp_sh_res():
@@ -20,16 +20,25 @@ def wsmp_sh_res():
     mantra_text = request.args.get('mantra')
     
     if not mantra_id or not mantra_text:
-        return jsonify({"error": "Missing input_id or input_text"}), 400
+#        return jsonify({"error": "Missing input_id or input_text"}), 400
+        response_json = {"error": "Missing input_id or input_text"}
+        response_json_str = json.dumps(response_json, ensure_ascii=False)
+        return Response(
+            response=response_json_str,
+            status=400,
+            mimetype='application/json'
+        )
     
     try:
-        sent_analysis = run_sh_text(cgi_file, mantra_text, "DN", lex="MW", 
+        cleaned_mantra = clean_all(mantra_text)
+        
+        sent_analysis = run_sh_text(cgi_file, cleaned_mantra, "DN", lex="MW", 
             us="f", output_encoding="deva", segmentation_mode="s", 
             text_type="t", stemmer="t")
         
         sent_analysis_str = json.dumps(sent_analysis, ensure_ascii=False)
         
-        sent_analysis_json, status = generate_results(mantra_id, mantra_text, sent_analysis_str, "sent")
+        sent_analysis_json, status = generate_results(mantra_id, cleaned_mantra, sent_analysis_str, "sent")
         
         response_json = json.dumps(sent_analysis_json, ensure_ascii=False)
         
@@ -40,9 +49,16 @@ def wsmp_sh_res():
         )
     
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+#        return jsonify({"error": str(e)}), 500
+        response_json = {"error": str(e)}
+        response_json_str = json.dumps(response_json, ensure_ascii=False)
+        return Response(
+            response=response_json,
+            status=500,
+            mimetype='application/json'
+        )
         
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=80)
 
